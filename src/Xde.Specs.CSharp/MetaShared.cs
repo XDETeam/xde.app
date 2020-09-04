707d33ce9b7464f49bdab5076a74b6818a3af28f
stan <stanislav@yarmonov.pro>
2020-09-04 18:20:53 +0300

    +Shared meta information.

---
+++ src/Xde.Specs.CSharp/MetaShared.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+//TODO:Provide shared information
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyProduct("XDE.app")]
+[assembly: AssemblyCompany("XDE.Team")]
+[assembly: AssemblyCopyright("2020 © XDE.Team")]
