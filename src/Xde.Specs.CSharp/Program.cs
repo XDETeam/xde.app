6e6b8ad351c93faeb768fe98a127a1f228c6731e
stan <stanislav@yarmonov.pro>
2020-09-08 17:47:45 +0300

    +Mesh entity for research purposes.

--- src/Xde.Specs.CSharp/Program.cs
+++ src/Xde.Specs.CSharp/Program.cs
@@ -1,4 +1,27 @@
 ﻿using System;
 using System.Reflection;
+using Xde.Mesh;
 
 Console.WriteLine($"XDE Specs {Assembly.GetEntryAssembly().GetName().Version}");
+
+var entity1 = Entity
+    .Create()
+    .Set<Http>()
+    .Set<Route>()
+;
+
+var entity2 = Entity.Create();
+
+Console.WriteLine(entity1);
+Console.WriteLine(entity2);
+Console.WriteLine(entity1.Get<Http>().Method);
+
+public class Http
+{
+    public string Method { get; set; }
+}
+
+public class Route
+{
+    public string Path { get; set; }
+}
