7af8bb72b54794cd1ca0523f851ba669073bbcb4
stan <stanislav@yarmonov.pro>
2020-10-16 09:15:54 +0300

    *Postgres dump.

---
+++ ops/postgres/dump.ps1
@@ -0,0 +1,19 @@
+﻿Clear-Host
+
+Set-Location $PSScriptRoot
+
+$settings = @{}
+Get-Content -Path ../.env | ForEach-Object {
+    $item = $_ -split '='
+    $settings[$item[0]] = $item[1]
+}
+
+if ($settings.POSTGRES_PASSWORD) {
+    $Env:PGPASSWORD = $settings.POSTGRES_PASSWORD
+}
+
+Start-Process `
+    -FilePath "$($env:ProgramFiles)\PostgreSQL\13\bin\pg_dump.exe" `
+    -Args @("-h", "localhost", "-p", "8132", "-d", "xde", "-U", "postgres") `
+    -RedirectStandardOutput "c:\Users\stani\OneDrive\Ego\xde-$([datetime]::Now.ToString("yyyyMMdd-hhmmssfff")).sql" `
+    -NoNewWindow
