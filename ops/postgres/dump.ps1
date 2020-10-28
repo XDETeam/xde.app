19052b266b72838815a482b26c7deb387b92ae1a
stan <stanislav@yarmonov.pro>
2020-10-28 14:57:53 +0200

    *Backup variations.

--- ops/postgres/dump.ps1
+++ ops/postgres/dump.ps1
@@ -15,5 +15,19 @@ if ($settings.POSTGRES_PASSWORD) {
 Start-Process `
     -FilePath "$($env:ProgramFiles)\PostgreSQL\13\bin\pg_dump.exe" `
     -Args @("-h", "localhost", "-p", "8132", "-d", "xde", "-U", "postgres") `
-    -RedirectStandardOutput "c:\Users\stani\OneDrive\Ego\xde-$([datetime]::Now.ToString("yyyyMMdd-hhmmssfff")).sql" `
+    -RedirectStandardOutput 'c:\Projects\sprint\mess.sql' `
     -NoNewWindow
+
+<#
+Start-Process `
+    -FilePath "$($env:ProgramFiles)\PostgreSQL\13\bin\pg_dump.exe" `
+    -Args @("-h", "localhost", "-p", "8132", "-d", "xde", "-U", "postgres") `
+    -RedirectStandardOutput "c:\Users\stani\OneDrive\Ego\xde-$([datetime]::Now.ToString("yyyyMMdd-HHmmssfff")).sql" `
+    -NoNewWindow
+
+Start-Process `
+    -FilePath "$($env:ProgramFiles)\PostgreSQL\13\bin\pg_dump.exe" `
+    -Args @("-h", "localhost", "-p", "8132", "-d", "xde", "-U", "postgres", "-F", "d", "-f", "c:\Users\stani\OneDrive\Ego\xde", "--compress=0") `
+	-RedirectStandardError "c:\Users\stani\OneDrive\Ego\xde.err" `
+    -NoNewWindow
+#>
