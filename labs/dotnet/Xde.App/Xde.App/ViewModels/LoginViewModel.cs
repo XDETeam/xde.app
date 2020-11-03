67d86ae3eeed8c4e0e184f31e69f0466afb4f8b2
stan <stanislav@yarmonov.pro>
2020-11-03 15:02:35 +0200

    +Water section boilerplate.

--- labs/dotnet/Xde.App/Xde.App/ViewModels/LoginViewModel.cs
+++ labs/dotnet/Xde.App/Xde.App/ViewModels/LoginViewModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Xamarin.Forms;
 using Xde.App.Views;
 
 namespace Xde.App.ViewModels
 {
-    public class LoginViewModel : BaseViewModel
+	public class LoginViewModel : BaseViewModel
     {
         public Command LoginCommand { get; }
 
@@ -18,7 +15,7 @@ namespace Xde.App.ViewModels
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            await Shell.Current.GoToAsync($"//{nameof(WaterPage)}");
         }
     }
 }
--- labs/dotnet/Xde.App/Xde.App/ViewModels/WaterViewModel.cs
+++ labs/dotnet/Xde.App/Xde.App/ViewModels/WaterViewModel.cs
@@ -1,18 +1,21 @@
-﻿using System;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Xde.App.ViewModels
 {
-    public class AboutViewModel : BaseViewModel
+	public class WaterViewModel : BaseViewModel
     {
-        public AboutViewModel()
+        public WaterViewModel()
         {
-            Title = "About";
+            Title = "Water";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 
+		public int Volume { get; set; } = DefaultAmount;
+
         public ICommand OpenWebCommand { get; }
+
+		public const int DefaultAmount = 300;
     }
 }
