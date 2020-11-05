fdcff739277f8c19d20273cb5893fb5538e5d254
stan <stanislav@yarmonov.pro>
2020-11-05 12:42:54 +0200

    +Mesh node table boilerplate.

---
+++ labs/pg/mesh/node/node.sql
@@ -0,0 +1,17 @@
+CREATE TABLE mesh.node (
+    id mesh.id NOT NULL DEFAULT mesh.id_new(),
+        CONSTRAINT pk_mesh_node PRIMARY KEY(id),
+
+	owner mesh.id NULL,
+		CONSTRAINT
+			fk_mesh_node_owner
+		FOREIGN KEY(
+			owner
+		) REFERENCES mesh.node (
+			id
+		)
+
+	-- TODO: This fields may be a part of lifetime aspect.
+    -- created timestamp NOT NULL,
+    -- deleted timestamp NULL
+) WITHOUT OIDS;
--- labs/pg/mesh/schema.sql
+++ labs/pg/mesh/schema.sql
@@ -3,3 +3,5 @@ CREATE SCHEMA IF NOT EXISTS mesh;
 \ir id.sql
 \ir id_sequence.sql
 \ir id_new.sql
+
+\ir node/node.sql
