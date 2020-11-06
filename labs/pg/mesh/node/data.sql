9b976963c4fcf2957a82a1ccf5ce44a1b8106285
stan <stanislav@yarmonov.pro>
2020-11-06 21:19:04 +0200

    *More sophisticated XSLT transformation.

--- labs/pg/mesh/node/data.sql
+++ labs/pg/mesh/node/data.sql
@@ -3,20 +3,43 @@ INSERT INTO mesh.node (
     path,
 	content
 )
-VALUES 
+VALUES
 (
 '//xde/default.xsl',
 '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+    <xsl:output omit-xml-declaration="yes" />
+
 	<xsl:template match="/">
-		TODO:
+        <section>
+            <h2>Meta links</h2>
+
+            <xsl:for-each select="//link">
+                <p>
+                    <xsl:text>@rel: </xsl:text>
+                    <xsl:value-of select="@rel"/>
+
+                    <xsl:text> @href: </xsl:text>
+                    <xsl:value-of select="@href"/>
+
+                    <xsl:text> @type: </xsl:text>
+                    <xsl:value-of select="@type"/>
+
+                    <xsl:text> @media: </xsl:text>
+                    <xsl:value-of select="@media"/>
+                </p>
+            </xsl:for-each>
+        </section>
 	</xsl:template>
 </xsl:stylesheet>'
 ),
 
 (
 '//xde/labs/db-version',
-'<root></root>'
+'<node>
+    <link rel="mesh:xslt" href="//xde/default.xsl" type="text/xsl" media="screen"/>
+</node>'
 )
 ;
 
-select mesh.node_xslt_transform('//xde/labs/db-version', '//xde/default.xsl');
+--TODO:0
+select mesh.node_xslt_transform('//xde/labs/db-version', 'screen');
--- labs/pg/mesh/node/node_xslt_transform.sql
+++ labs/pg/mesh/node/node_xslt_transform.sql
@@ -1,13 +1,32 @@
-CREATE OR REPLACE FUNCTION mesh.node_xslt_transform(node text, style text)
+CREATE OR REPLACE FUNCTION mesh.node_xslt_transform(node text, media text)
 RETURNS text
 AS $$
 DECLARE
     _document xml;
+    _xpath text;
+    _links text[];
     _style xml;
     _result xml;
 BEGIN
     _document = (SELECT content FROM mesh.node WHERE path = node_xslt_transform.node);
-    _style = (SELECT content FROM mesh.node WHERE path = node_xslt_transform.style);
+
+    _xpath = format(
+        '//link[@rel="mesh:xslt" and @type="text/xsl" and @media="%s"]/@href',
+        node_xslt_transform.media
+    );
+    _links = xpath(_xpath, _document);
+
+    CASE cardinality(_links)
+        WHEN 0 THEN RETURN NULL;
+        WHEN 2 THEN RAISE EXCEPTION 'Multiple XSLT styles defined';
+        ELSE
+    END CASE;
+
+    _style = (SELECT content FROM mesh.node WHERE path = _links[1]);
+    IF _style IS NULL THEN
+        RAISE EXCEPTION 'XSLT style "%" not found', _links[1];
+    END IF;
+
     _result = xslt_process(_document::text, _style::text, '');
 
     RETURN _result;
