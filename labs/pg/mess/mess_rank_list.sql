89740c16a0a330e511265f5d664ce054a81b8954
stan <stanislav@yarmonov.pro>
2020-11-06 11:48:28 +0200

    +DB mess rank list.

---
+++ labs/pg/mess/mess_rank_list.sql
@@ -0,0 +1,16 @@
+CREATE VIEW mesh.mess_rank_list
+AS SELECT
+	rank.index,
+	mess.id,
+	rank.title,
+	mess.content
+FROM
+	mesh.mess,
+	LATERAL XMLTABLE(
+		'//rank'::text PASSING (mess.content) COLUMNS
+		index integer PATH ('@index'::text),
+		title text PATH ('../@title'::text)
+	) rank
+ORDER BY
+	rank.index
+;
--- labs/pg/mess/schema.sql
+++ labs/pg/mess/schema.sql
@@ -1,1 +1,2 @@
 \ir mess_deadline_list.sql
+\ir mess_rank_list.sql
