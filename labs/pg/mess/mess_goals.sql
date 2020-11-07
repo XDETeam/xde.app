17e3300ab7285175e473677796942dbda2189c5c
stan <stanislav@yarmonov.pro>
2020-11-07 10:36:43 +0200

    *DB mess goals content rendered.

--- labs/pg/mess/mess_goals.sql
+++ labs/pg/mess/mess_goals.sql
@@ -3,7 +3,7 @@ AS SELECT
 	'stan.body.water'::text AS goal,
     (mess_drink_goals.value * 100)::integer::text || '%'::text AS targets,
     'Выпить воды'::text AS notes,
-    NULL::xml AS content
+	(SELECT content FROM mesh.mess WHERE id = 'stan.body.water') AS content
 FROM
 	mesh.mess_drink_goals
 WHERE
@@ -16,7 +16,7 @@ UNION ALL SELECT
 		((((100 * sum(mess_gmg_done_days.hours))::double precision / (((7000.00 / 40)::double precision * (t.current - t.start)) / (t.finish - t.start))))::integer)::text || '%'::text
 	) AS targets,
 	NULL::text AS notes,
-    NULL::xml AS content
+    (SELECT content FROM mesh.mess WHERE id = 'gmg.invoice') AS content
 FROM
 	mesh.mess_gmg_done_days,
     (
@@ -33,28 +33,28 @@ GROUP BY
 	t.current
 
 UNION ALL SELECT
-	(mess_deadline_list.id)::text AS goal,
-	(mess_deadline_list.at)::text AS targets,
-    mess_deadline_list.title AS notes,
-    NULL::xml AS content
+	id::text AS goal,
+	at::text AS targets,
+    title AS notes,
+    content AS content
 FROM
 	mesh.mess_deadline_list
 WHERE
 	(mess_deadline_list.at < ((now())::date + 1))
 
 UNION ALL SELECT
-	(mess_rank_list.id)::text AS goal,
-	(mess_rank_list.index)::text AS targets,
-	mess_rank_list.title AS notes,
-    NULL::xml AS content
+	id::text AS goal,
+	index::text AS targets,
+	title AS notes,
+    content AS content
 FROM
 	mesh.mess_rank_list
 
 UNION ALL SELECT
-	(mess_depends_list.id)::text AS goal,
-	(mess_depends_list.count)::text AS targets,
+	id::text AS goal,
+	count::text AS targets,
 	'deps'::text AS notes,
-    NULL::xml AS content
+    content AS content
 FROM
 	mesh.mess_depends_list
 ;
