36e678b306c1c9ddb2b67cba1ee1a067e2765f6c
stan <stanislav@yarmonov.pro>
2021-03-28 21:40:37 +0300

    +Logging notes.

---
+++ labs/dotnet/Xde.Specs.CSharp/Flow/Log/NamespaceDoc.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+
+namespace Xde.Flow.Log
+{
+	/// <summary>
+	/// TODO: Logging
+	/// </summary>
+	/// 
+	/// <remarks>
+	/// Logs usually are considered as renderers that flatten structured data into text files.
+	/// This might be stupid from both writing log perspective and further logs analysis.
+	/// Probably they should by structured, aspected (log partition that contains errors only
+	/// for example). And then log becomes a structured storage which is "append-only"?
+	/// </remarks>
+	[CompilerGenerated]
+	internal class NamespaceDoc
+	{
+
+	}
+}
