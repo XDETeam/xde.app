e357f81ce2414dc5b30b9794e984f272a4b4bf31
stan <stanislav@yarmonov.pro>
2021-03-28 21:55:06 +0300

    *Parsing notes.

--- labs/dotnet/Xde.Specs.CSharp/Flow/Parse/NamespaceDoc.cs
+++ labs/dotnet/Xde.Specs.CSharp/Flow/Parse/NamespaceDoc.cs
@@ -10,8 +10,9 @@ namespace Xde.Flow.Parse
 	/// <para>
 	/// TODO:What is responsibility of the parser? Compared to different transformers,
 	/// processorts, generators, etc. Probably parser is converting non-structured into
-	/// structured (tokens into structures). Generator - vice versa. And transformers
-	/// converting structures.
+	/// structured (tokens into structures). Generator - vice versa (maybe not a good
+	/// idea because generators should create from the ground, maybe renderers). And
+	/// transformers converting structures.
 	/// </para>
 	/// <para>
 	/// Parser should not respond for generating DOM models. It can be streaming (e.g.
@@ -20,6 +21,16 @@ namespace Xde.Flow.Parse
 	/// <para>
 	/// Using <see cref="IEnumerable"/> as input/ouput looks obvious. But performance is
 	/// important for this.
+	/// </para>
+	/// <para>
+	/// There is an opinion that parsers and validators are tightly coupled. Probably
+	/// parsing is more important because it converts into structure and structure has a
+	/// validity criterias. Or parser simply emits some messages as a response to signals?
+	/// So acts like a decoder and the renderers become encoders?
+	/// </para>
+	/// <para>
+	/// Parser should be reusable. And data behind parsers too. For example arithmetic
+	/// operations should not be a part of every specific language.
 	/// </para>
 	/// </remarks>
 	[CompilerGenerated]
