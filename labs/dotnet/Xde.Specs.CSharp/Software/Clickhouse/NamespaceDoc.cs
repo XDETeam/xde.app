1c2b6661617680460a762c98f60849b470e1c4fd
stan <stanislav@yarmonov.pro>
2021-04-10 10:41:19 +0300

    +Yandex Clickhouse.

---
+++ labs/dotnet/Xde.Specs.CSharp/Software/Clickhouse/NamespaceDoc.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+
+namespace Xde.Software.Clickhouse
+{
+	/// <summary>
+	/// Yandex Clickhouse
+	/// </summary>
+	/// 
+	/// <remarks>
+	/// TODO: https://clickhouse.tech/
+	/// TODO: IDatabase? IColumnarDatabase?
+	/// </remarks>
+	[CompilerGenerated]
+    internal class NamespaceDoc
+    {
+
+    }
+}
--- labs/dotnet/Xde.Specs.CSharp/Software/NamespaceDoc.cs
+++ labs/dotnet/Xde.Specs.CSharp/Software/NamespaceDoc.cs
@@ -8,10 +8,11 @@ namespace Xde.Software
 	/// 
 	/// <remarks>
 	/// TODO:This namespace is reponsible for existing software products definitions and
-	/// specifications.
+	/// specifications. That can be useful for the integration purposes.
 	/// TODO: Initially simply collect software that can be of interested, then try to define
 	/// some contracts and attempt to sort out.
-	/// TODO: Define software as compositions/features? As it can be for developed software?
+	/// TODO: Define software as compositions/features? As it can be for software developed
+	/// using XDE? This approach can also replace interfaces.
 	/// </remarks>
 	[CompilerGenerated]
 	internal class NamespaceDoc
