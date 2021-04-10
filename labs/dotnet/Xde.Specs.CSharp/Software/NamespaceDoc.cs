23526e0f184d4b3662025d9966bc9461d5c0cfbc
stan <stanislav@yarmonov.pro>
2021-04-10 10:34:03 +0300

    +Visual Studio

--- labs/dotnet/Xde.Specs.CSharp/Software/NamespaceDoc.cs
+++ labs/dotnet/Xde.Specs.CSharp/Software/NamespaceDoc.cs
@@ -9,6 +9,9 @@ namespace Xde.Software
 	/// <remarks>
 	/// TODO:This namespace is reponsible for existing software products definitions and
 	/// specifications.
+	/// TODO: Initially simply collect software that can be of interested, then try to define
+	/// some contracts and attempt to sort out.
+	/// TODO: Define software as compositions/features? As it can be for developed software?
 	/// </remarks>
 	[CompilerGenerated]
 	internal class NamespaceDoc
