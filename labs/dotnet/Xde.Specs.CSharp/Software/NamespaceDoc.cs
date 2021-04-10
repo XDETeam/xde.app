d113b73578bdee5d4c42045780213ed5599b9a04
stan <stanislav@yarmonov.pro>
2021-04-10 10:25:35 +0300

    +Software namespace.

---
+++ labs/dotnet/Xde.Specs.CSharp/Software/NamespaceDoc.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+
+namespace Xde.Software
+{
+	/// <summary>
+	/// Software specifications
+	/// </summary>
+	/// 
+	/// <remarks>
+	/// TODO:This namespace is reponsible for existing software products definitions and
+	/// specifications.
+	/// </remarks>
+	[CompilerGenerated]
+	internal class NamespaceDoc
+	{
+
+	}
+}
