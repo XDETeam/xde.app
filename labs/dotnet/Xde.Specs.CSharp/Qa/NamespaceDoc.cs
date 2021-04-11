bb504c98c182942b2e974bc585ebca36d918d76c
stan <stanislav@yarmonov.pro>
2021-04-11 22:29:08 +0300

    +QA notes.

---
+++ labs/dotnet/Xde.Specs.CSharp/Qa/NamespaceDoc.cs
@@ -0,0 +1,21 @@
+using System.Runtime.CompilerServices;
+
+namespace Xde.Qa
+{
+	/// <summary>
+	/// Quality assurance
+	/// </summary>
+	/// 
+	/// <remarks>
+	/// - This can become a "Specs" namespace?
+	/// - For specs there is one interesting requirement. Let's say we have some subsystem
+	/// that have a bunch of tests/specs. And we want to compare several implementations of
+	/// this subsystem. How to ensure that all tests were implemented. Abstract class with
+	/// a lot of test methods is not a good option.
+	/// </remarks>
+	[CompilerGenerated]
+	internal class NamespaceDoc
+	{
+
+	}
+}
