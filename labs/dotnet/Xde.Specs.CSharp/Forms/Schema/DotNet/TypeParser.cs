ca6bfa2d31cded782e965b2fea95ecd7316c6fcb
stan <stanislav@yarmonov.pro>
2021-04-17 19:57:08 +0300

    +Basic type to form parsing.

--- labs/dotnet/Xde.Specs.CSharp/Forms/Schema/DotNet/TypeParser.cs
+++ labs/dotnet/Xde.Specs.CSharp/Forms/Schema/DotNet/TypeParser.cs
@@ -1,12 +1,46 @@
 using System;
+using System.Linq;
+using Xde.Forms.Flow.Parse;
 
 namespace Xde.Forms.Schema.DotNet
 {
+	/// <summary>
+	/// Parser from <see cref="System.Type"/> into <see cref="Form"/>
+	/// </summary>
 	public class TypeParser
+		: IParser<Type, Form>
 	{
-		public Form Parse(Type type)
+		/// <summary>
+		/// TODO:Parsing function
+		/// </summary>
+		Form IParser<Type, Form>.Parse(Type type)
 		{
-			throw new NotImplementedException();
+			_ = type ?? throw new ArgumentNullException(nameof(type));
+
+			var name = new Fullname()
+			{
+				Name = type.Name,
+				Namespace = type.Namespace,
+				Layer = type.AssemblyQualifiedName
+			};
+
+			var features = type
+				.GetProperties()
+				.Select(property => new Feature()
+				{
+					Name = property.Name,
+					//TODO:0 Registry
+					Form = (this as IParser<Type, Form>).Parse(property.PropertyType)
+				})
+			;
+
+			var result = new Form()
+			{
+				Fullname = name,
+				Features = features
+			};
+
+			return result;
 		}
 	}
 }
--- labs/dotnet/Xde.Specs.CSharp/Forms/Schema/DotNet/TypeParserSpecs.cs
+++ labs/dotnet/Xde.Specs.CSharp/Forms/Schema/DotNet/TypeParserSpecs.cs
@@ -1,7 +1,17 @@
+using System;
+using Xde.Forms.Flow.Parse;
 using Xunit;
 
 namespace Xde.Forms.Schema.DotNet
 {
+	/// <summary>
+	/// TODO:<see cref="TypeParser"/> specs
+	/// </summary>
+	/// 
+	/// <remarks>
+	/// TODO:It would be nice to implement abstractions for parsers, because most of tests
+	/// will be the same. And maybe implement class-per-test scenario.
+	/// </remarks>
 	public class TypeParserSpecs
 	{
 		public class Sample
@@ -12,11 +22,36 @@ namespace Xde.Forms.Schema.DotNet
 		}
 
 		[Fact]
+		public void Parse_TypeIsNull_ThrowException()
+		{
+			IParser<Type, Form> parser = new TypeParser();
+			
+			var e = Assert.Throws<ArgumentNullException>(() => parser.Parse(null));
+
+			Assert.Equal("type", e.ParamName);
+		}
+
+		[Fact]
 		public void Parse_SampleType_ProperForm()
 		{
-			var parser = new TypeParser();
+			IParser<Type, Form> parser = new TypeParser();
+			
 			var form = parser.Parse(typeof(Sample));
+
 			Assert.NotNull(form);
+
+			var type = typeof(Sample);
+			Assert.Equal(type.Name, form.Fullname.Name);
+			Assert.Equal(type.Namespace, form.Fullname.Namespace);
+			Assert.Equal(type.AssemblyQualifiedName, form.Fullname.Layer);
+
+			Assert.NotNull(form.Features);
+			Assert.Collection(
+				form.Features,
+				//TODO:Add item.Form comparisons that should be reused from the registry
+				item => Assert.Equal(nameof(Sample.Id), item.Name),
+				item => Assert.Equal(nameof(Sample.Name), item.Name)
+			);
 		}
 	}
 }
