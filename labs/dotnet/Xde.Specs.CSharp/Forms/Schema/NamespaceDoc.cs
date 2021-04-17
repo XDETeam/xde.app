60f4fc23a347ab8575606a350f6b9b97632e6be9
stan <stanislav@yarmonov.pro>
2021-04-17 17:57:51 +0300

    +Schemas management introduced.

---
+++ labs/dotnet/Xde.Specs.CSharp/Forms/Schema/NamespaceDoc.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+
+namespace Xde.Forms.Schema
+{
+	/// <summary>
+	/// Data schemas
+	/// </summary>
+	/// 
+	/// <remarks>
+	/// TODO:Defines forms for data schemas and conversion routines (get schema from
+	/// some source or generate schema into other format).
+	/// </remarks>
+	[CompilerGenerated]
+	internal class NamespaceDoc
+	{
+
+	}
+}
