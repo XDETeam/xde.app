47734a15909b3f3150a0d8a05782e28448f7f09f
stan <stanislav@yarmonov.pro>
2021-06-13 18:24:01 +0300

    *Slight refactoring of mess.node_review

--- labs/pg/mess/mess_node_review.sql
+++ labs/pg/mess/mess_node_review.sql
@@ -1,31 +1,22 @@
 CREATE OR REPLACE FUNCTION mess.node_review(
-    _operator text = '~',
-    _filter text
+    _filter text = NULL,
+    _operator text = '@'
 )
-RETURNS TABLE (
-    "url" ltree,
-    "content" xml
-)
+RETURNS SETOF mess.mesh
 AS $$
-BEGIN
     SELECT
-        url,
-        xt.content
-    FROM
-        mess.mesh,
-        XMLTABLE('//*[not(ancestor::story)]' PASSING content COLUMNS "content" XML PATH '.') xt
-    WHERE
-        _filter IS NULL OR
-            (CASE _operator
-                WHEN '~' THEN url ~ _filter::lquery
-                WHEN '@' THEN url @ _filter::ltxtquery
-                ELSE url = _filter::ltree
-            END)
+            url,
+           xt.content
+    FROM mess.mesh,
+         XMLTABLE('//*[not(ancestor::story)]' PASSING content COLUMNS "content" XML PATH '.') xt
+    WHERE _filter IS NULL
+       OR (CASE _operator
+               WHEN '~' THEN url ~ _filter::lquery
+               WHEN '@' THEN url @ _filter::ltxtquery
+               ELSE url = _filter::ltree
+        END)
     ORDER BY
-		-- TODO: Perf
+        -- TODO: Perf
         random()
-    LIMIT
-        1
-    ;
-END
-$$ LANGUAGE plpgsql;
+    LIMIT 1;
+$$ LANGUAGE sql;
