5a299f5c151adde1a540e44b1606ec2e546bddc1
stan <stanislav@yarmonov.pro>
2021-06-30 21:37:56 +0300

    +Debit experiments in PG.

--- labs/pg/mesh/mess/balance_list.sql
+++ labs/pg/mesh/mess/balance_list.sql
@@ -5,7 +5,7 @@ AS SELECT
     credit.unit,
     credit.lot,
     credit.cost,
-    cost * lot as value
+    COALESCE(value, cost * lot) as value
 FROM
     mesh.node_view,
     LATERAL XMLTABLE(('//credit'::text) PASSING (node_view.content) COLUMNS
@@ -14,6 +14,26 @@ FROM
         "cost" numeric PATH('@cost'),
         "unit" text PATH('@unit'),
         "lot" numeric PATH('@lot'),
+        "value" numeric PATH('@value'),
         "title" text PATH('text()')
 	) credit
+
+UNION ALL SELECT
+    debit.on,
+    trim(BOTH E' \n\r\t' FROM title) as title,
+    debit.unit,
+    debit.lot,
+    debit.cost,
+    -COALESCE(value, cost * lot) as value
+FROM
+    mesh.node_view,
+    LATERAL XMLTABLE(('//debit'::text) PASSING (node_view.content) COLUMNS
+        "to" ltree PATH('@to'),
+        "on" date PATH('@on'),
+        "cost" numeric PATH('@cost'),
+        "unit" text PATH('@unit'),
+        "lot" numeric PATH('@lot'),
+        "value" numeric PATH('@value'),
+        "title" text PATH('text()')
+	) debit
 ;
