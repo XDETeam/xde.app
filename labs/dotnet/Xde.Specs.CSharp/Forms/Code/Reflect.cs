f5dbf62101becc6b1e98f61c5749de36e8e85c3b
stan <stanislav@yarmonov.pro>
2021-07-02 19:57:06 +0300

    +Reflection filtering boilerplate.

--- labs/dotnet/Xde.Specs.CSharp/Forms/Code/Reflect.cs
+++ labs/dotnet/Xde.Specs.CSharp/Forms/Code/Reflect.cs
@@ -1,10 +1,51 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Xde.Forms.Code
 {
 	/// TODO:Helper tool for reflection routines
 	public class Reflect
 	{
+		private static readonly Reflect Current = new();
+
+		public interface IFilter
+			: IEnumerable<Type>
+		{
+
+		}
+
+		private class Filter
+			: IFilter
+		{
+			public Filter(Assembly assembly)
+			{
+
+			}
+
+			IEnumerator<Type> IEnumerable<Type>.GetEnumerator()
+			{
+				throw new NotImplementedException();
+			}
+
+			IEnumerator IEnumerable.GetEnumerator()
+				=> (this as IEnumerable<Type>).GetEnumerator()
+			;
+		}
+
+		public static IFilter Assembly<T>() => new Filter(typeof(T).Assembly);
+
+		static Reflect()
+		{
+
+		}
+
+		private Reflect()
+		{
+
+		}
+
 		/// <summary>
 		/// Check if <paramref name="type"/> can be created
 		/// </summary>
