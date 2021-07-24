ccd7bdabbfa97f93289878b8943836efe21fae7b
stan <stanislav@yarmonov.pro>
2021-07-24 17:26:38 +0300

    +Spins section boilerplate.

---
+++ labs/dotnet/Xde.App/Xde.App/Views/Spins/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xde.App.Views.Spins
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
