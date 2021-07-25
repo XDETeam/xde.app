a4d01b1401a0ce7f1ca3155670e5de3c4f53f878
stan <stanislav@yarmonov.pro>
2021-07-25 17:50:00 +0300

    +Initial version of spin action.

---
+++ labs/pg/mesh/mess/spin_do.sql
@@ -0,0 +1,45 @@
+create or replace procedure mesh.spin_do(
+    _url text,
+    _content text = null
+)
+language plpgsql
+as $$
+declare
+    __existing bigint;
+    __author text = current_user;
+    __now timestamp = current_timestamp;
+begin
+    select
+        id
+    into
+        __existing
+    from
+        mess.spin
+    where
+        author = __author
+    ;
+
+    -- Close existing spin
+    if __existing is not null then
+        update
+            mess.spin
+        set
+            content = _content
+        where
+            id = __existing
+        ;
+    end if;
+
+    -- Open new spin
+    insert into mess.spin(
+        chain,
+        author,
+        url,
+        visited
+    ) values (
+        __existing,
+        __author,
+        _url,
+        __now
+    );
+end $$;
--- labs/pg/mess/spin.sql
+++ labs/pg/mess/spin.sql
@@ -1,5 +1,38 @@
 create table mess.spin (
+    id bigserial not null,
+    chain bigint null,
     author text not null,
     url text not null,
+    content text null,
     visited timestamp not null
+);
+
+alter table
+    mess.spin
+add constraint
+    pk_mess_spin
+primary key (
+    id
+);
+
+alter table
+    mess.spin
+add constraint
+    fk_mess_spin_chain
+foreign key (
+    chain
+) references mess.spin (
+    id
+);
+
+create index
+    ix_mess_spin_url
+on mess.spin (
+    url
+);
+
+create index
+    ix_mess_spin_chain
+on mess.spin (
+    chain
 );
